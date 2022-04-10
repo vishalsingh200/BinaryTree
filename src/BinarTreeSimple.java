1fce0dbe8de876b95546f08b43c6f3fc742e82ff
Vishal Kumar Singh <singhvishal0847@gmail.com>
2022-04-10 13:40:30 +0530

    initial commit

--- src/BinarTreeSimple.java
+++ src/BinarTreeSimple.java
@@ -1,2 +1,35 @@
 public class BinarTreeSimple {
+    static class Node
+    {
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int item)
+        {
+            data = item;
+            left = right = null;
+        }
+    }
+
+        Node root;
+        BinarTreeSimple(int key)
+        {
+            root = new Node(key);
+        }
+
+        BinarTreeSimple()
+        {
+            root = null;
+        }
+
+        public static void main(String[] args)
+        {
+            BinarTreeSimple tree = new BinarTreeSimple();
+            tree.root = new Node(1);
+            tree.root.left = new Node(2);
+            tree.root.right = new Node(3);
+            tree.root.left.left = new Node(4);
+        }
+//
 }
